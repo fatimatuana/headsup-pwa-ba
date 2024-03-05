385507b662d0a67ba010235fd53bd3d64f956a86
fatima tuana <fatima__tuana@hotmail.com>
2024-03-05 19:09:21 +0100

    generak structure

--- headsup-server/index.js
+++ headsup-server/index.js
@@ -1,8 +1,39 @@
+// const express = require('express');
+// const app = express();
+
+// app.get('/', (req, res) => {
+//   res.send('Hello, World!');
+// });
+
+// const port = process.env.PORT || 3000;
+// app.listen(port, () => {
+//   console.log(`Server listening on port ${port}`);
+// });
+//************************** */
+// const express = require('express');
+// const path = require('path');
+// const app = express();
+
+// app.use(express.static(path.join(__dirname, 'public')));
+
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '/app/public/index.html'));
+// });
+
+// const port = process.env.PORT || 3000;
+// app.listen(port, () => {
+//   console.log(`Server listening on port ${port}`);
+// });
+//******************************** */
 const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
+  res.send('HOME - API response');
+});
+
+app.get('/api', (req, res) => {
+  res.send('API response');
 });
 
 const port = process.env.PORT || 3000;
