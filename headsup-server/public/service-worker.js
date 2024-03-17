7126e7241a779de7f1816b25a73bd749765a6a3c
fatima tuana <fatima__tuana@hotmail.com>
2024-03-17 20:59:26 +0100

    some basic changes

--- headsup-server/public/service-worker.js
+++ headsup-server/public/service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/precache-manifest.61b23f0f39cf9f2d22cfae6e6e071558.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/precache-manifest.0ebff50382c6e33fb2c6cc085284bae1.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 
 if (workbox) {
